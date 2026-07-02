968cac51c140e00f7dc229eab762dac2cb926fc2
2410040 <2410040@i-seifu.jp>
2026-07-02 15:47:00 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+  <div class="logo" id="headerLogo" style="cursor: pointer;">
+    <img src="艇王.jpg" alt="艇王 BOATRACE ROYAL" style="height: 44px; width: auto; display: block;">
+  </div>
+  <div class="header-right">
+    <div class="header-date" id="headerDate">----年--月--日 (-)</div>
+    <div class="header-auth" id="headerAuth">
+    </div>
+  </div>
+</header>
